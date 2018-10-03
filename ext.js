857c0d58889c6a4e961453bc4917f1561f274b5a
kunal <kunalgk1999@gmail.com>
2018-10-03 17:49:54 +0530

    updated readMe

--- ext.js
+++ ext.js
@@ -1,6 +1,20 @@
+function createTodo(t){
+    html = `
+                            <div class="uk-margin uk-card uk-card-primary uk-card-hover uk-light uk-padding">
+                                <div class="uk-body">`;
+    html += t;
+    html += `<br><br>
+                                    <button class="uk-button uk-button-secondary">delete</button>
+                                </div>
+                            </div>
+                        </div>
+                   `;
+    return html;
+}
+
 function addTodo(){
     var todo = $('#input').attr('placeholder');
-    console.log(todo);
+    $('#container').append(createTodo(todo));
 }
 
 $('document').ready(function() {
